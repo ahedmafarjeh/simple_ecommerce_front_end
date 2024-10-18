c7698d62008ca5cb51f747bc809c1e090d526414
ahedmafarjeh <ahedmafarjeh10@gmail.com>
2024-10-18 23:47:43 +0300

    add loader and pagination

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-
+const limit = 20; // limit is the number of products in each page
 const getData = async (url) =>{
   
   const {data} = await axios.get(url);
@@ -6,6 +6,9 @@ const getData = async (url) =>{
 }
 
 const displayCategories = async () =>{
+  const loader = document.querySelector(".loader-container");
+  loader.classList.add("active");
+  try{
   const categories = await getData('https://dummyjson.com/products/category-list');
   const result = categories.map( category =>{
     return `
@@ -17,11 +20,20 @@ const displayCategories = async () =>{
   }).join(' ');
   
   document.querySelector(".categories .row").innerHTML = result;
+  
+}catch(e){
+  document.querySelector(".categories .row").innerHTML = '<p style="color: white;">Error loading category</p>';
+}
+finally{
+  loader.classList.remove("active");
+}
 }
 
-const displayProducts = async () => {
-  const all_products = await getData('https://dummyjson.com/products');
-  
+const displayProducts = async (page = 1) => {
+  skip = (page -1) * limit; // to get the skip value which is the number of products that will be skipped
+  const all_products = await getData(`https://dummyjson.com/products?limit=20&skip=${skip}`);
+  const number_of_pages = Math.ceil(all_products.total / limit);
+  console.log(page);
   const result = all_products.products.map(product => {
   return `
       <div class="product"> 
@@ -31,8 +43,26 @@ const displayProducts = async () => {
       </div>
   `
   }).join(' ');
-  
+
+  // Pagination goes here
+  let pagination_links = ``;
+  if(page == 1){
+    pagination_links += `<li class="page-item"><button disabled)" class="page-link" >&laquo;</a></li>`;
+  }
+  else{
+    pagination_links += `<li class="page-item"><button onclick="displayProducts(${page-1})" class="page-link" >&laquo;</a></li>`;
+  }
+  for(let i = 1; i <= number_of_pages; i++){
+    pagination_links += `<li class="page-item ${i == page?'active':''}"><button onclick="displayProducts(${i})" class="page-link">${i}</a></li>`;
+  }
+  if (page == number_of_pages){
+    pagination_links += `<li class="page-item"><button class="page-link" disabled>&raquo;</a></li>`;
+  }
+  else{
+    pagination_links += `<li class="page-item"><button onclick="displayProducts(${parseInt(page)+1})" class="page-link" >&raquo;</a></li>`;
+  }
   document.querySelector(".products .row").innerHTML = result;
+  document.querySelector(".pagination").innerHTML = pagination_links;
 }
 
 displayCategories();
